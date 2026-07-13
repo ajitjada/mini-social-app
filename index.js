f2f16cf3c8d82fab8dbb9c6eb77e0e8e7c7d0352
ajitjada <ajitjada71@gmail.com>
2026-07-13 22:12:14 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,139 @@
+const express = require('express')
+const app = express()
+const userModel = require('./models/user')
+const postModel = require('./models/post')
+const cookieParser = require('cookie-parser')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const crypto = require('crypto')
+const path = require('path')
+const upload = require('./config/multerconfig')
+
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(cookieParser())
+
+
+app.get('/', (req, res) => {
+    res.render("index")
+})
+
+app.get('/profile/upload', (req, res) => {
+    res.render("profileupload")
+})
+
+app.post('/upload', isloggedIn, upload.single("image"), async (req, res) => {
+    let user = await userModel.findOne({email: req.user.email})
+    user.profilepic = req.file.filename
+    await user.save()
+    res.redirect('/profile')
+})
+
+app.get('/register', (req, res) => {
+    res.render("index")
+})
+
+app.post('/register', async (req, res) => {
+    let { email, username, name, password, age } = req.body
+    let user = await userModel.findOne({ email })
+    if (user) return res.status(500).send("User already registred")
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            let user = await userModel.create({
+                username,
+                name,
+                email,
+                age,
+                password: hash
+            })
+            let token = jwt.sign({ email: email, userid: user._id }, "tumtumtaunav")
+            res.cookie("token", token)
+            res.redirect("/login")
+        })
+    })
+})
+
+app.get('/login', (req, res) => {
+    res.render("login")
+})
+
+app.post('/login', async (req, res) => {
+    let { email, password } = req.body
+    let user = await userModel.findOne({ email }).populate("post")
+    if (!user) return res.status(500).send("Something went Wrong")
+
+    bcrypt.compare(password, user.password, function (err, result) {
+        if (result) {
+            let token = jwt.sign({ email: email, userid: user._id }, "tumtumtaunav")
+            res.cookie("token", token)
+            res.status(200).render("profile", {user})
+        }
+        else {
+            res.status(500).send("Something went Wrong")
+        }
+    
+    })
+})
+
+app.get('/profile', isloggedIn,async (req, res) => {
+    let user = await userModel.findOne({email: req.user.email}).populate("post")
+    console.log({user})
+    res.render("profile", {user})
+})
+
+app.post('/post', isloggedIn, async (req, res) => {
+    let user = await userModel.findOne({email: req.user.email})
+    let {content} = req.body
+
+    let post = await postModel.create({
+        user: user._id,
+        content
+    })
+
+    user.post.push(post._id)
+    await user.save()
+    res.redirect("/profile")
+})
+
+app.get('/like/:id', isloggedIn, async (req, res) => {
+    let post = await postModel.findOne({_id: req.params.id}).populate("user")
+    
+    if(post.likes.indexOf(req.user.userid) === -1) {
+        post.likes.push(req.user.userid)
+    } else {
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1)
+    }
+
+    await post.save()
+    res.redirect("/profile")
+})
+
+app.get('/edit/:id', isloggedIn, async (req, res) => {
+    let post = await postModel.findOne({_id: req.params.id}).populate("user")
+    res.render("edit", {post})
+})
+
+app.post('/update/:id', isloggedIn, async (req, res) => {
+    let post = await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content})
+    res.redirect("/profile")
+})
+
+
+app.get("/logout", (req, res) => {
+    res.cookie("token", "")
+    res.redirect("/login")
+})
+
+function isloggedIn(req, res, next) {
+    if(req.cookies.token === "") res.send("you must be logged in")
+    else {
+        let data = jwt.verify(req.cookies.token, "tumtumtaunav")
+        req.user = data
+        next()
+    }
+}
+
+app.listen(3000)
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/miniApp");
+
+const userSchema = mongoose.Schema({
+    username: String,
+    name: String,
+    email: String,
+    password: String,
+    age: Number,
+    profilepic: {
+        type: String,
+        defalut: "default.png"
+    },
+    post: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "post"
+        }
+    ]
+})
+
+module.exports = mongoose.model('user', userSchema)
